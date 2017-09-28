cc9a675f89cce679d2e7a39740e6f96fd0af9060
henucg <1825337674@qq.com>
2017-09-28 16:18:29 +0800

    快速排序修改

--- src/sort/QuickSortDemo01.java
+++ src/sort/QuickSortDemo01.java
@@ -14,32 +14,33 @@ public class QuickSortDemo01 {
 			while(low<high&&array[high]>=flag){
 				high-- ;
 			}
-			int tempLow = array[low] ;
 			array[low] = array[high] ;
-			array[high] = tempLow ;
 			
-			while(low<high&&array[low]<flag){
+			while(low<high&&array[low]<=flag){
 				low++ ;
 			}
-			int tempHigh = array[high] ;
 			array[high] = array[low] ;
-			array[low] = tempHigh ;
 		}
 		array[low] = flag ;
 		return low ;
 	}
 	
-	public static int[] sort(int[] array,int low,int high){
+	public static void sort(int[] array,int low,int high){
+		if(low>high){
+			return ;
+		}
+		
 		int midle = getMidle(array,low,high) ;
 		sort(array,low,midle-1) ;
 		sort(array,midle+1,high) ;
-		return array ;
 	}
 	
 	public static void main(String[] args) {
 		int[] array = {46,30,82,90,56,17,95,15} ;
-		array = sort(array,0,array.length-1) ;
+		sort(array,0,array.length-1) ;
 		
-		System.out.println(array.toString());
+		for(int i=0;i<array.length;i++){
+			System.out.print(array[i] + "  ");
+		}
 	}
 }
