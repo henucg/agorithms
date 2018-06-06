db79d509e0ea093caee6c4511be533bbd0e8c411
henucg <1825337674@qq.com>
2018-06-06 14:34:00 +0800

    算法测试

---
+++ src/map/Test01.java
@@ -0,0 +1,20 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		Map<String,Object> map = new HashMap<String,Object>() ;
+		map.put("k1", "v1") ;
+		map.put("k2", "v2") ;
+		
+		System.out.println(map.size());
+		
+		map.remove("k1") ;
+		
+		System.out.println(map.get("k1"));
+	}
+
+}
