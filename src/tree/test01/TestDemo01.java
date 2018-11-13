91bcead05df3aeef47774536bc9bc5563054017c
henucg <1825337674@qq.com>
2018-11-13 15:24:59 +0800

    树遍历

---
+++ src/tree/test01/TestDemo01.java
@@ -0,0 +1,83 @@
+package tree.test01;
+
+public class TestDemo01 {
+
+    public static String str = "" ;
+
+    public static void main(String[] args){
+        //构建树
+        TreeNode treeNode = new TreeNode("A",
+                new TreeNode("B",
+                        new TreeNode("D",null,
+                                new TreeNode("E",
+                                        new TreeNode("H",null,null),null)),null),
+                new TreeNode("C",
+                        new TreeNode("F",
+                                new TreeNode("G",null,null),null),null)) ;
+
+        //遍历
+        System.out.println("先序遍历：" + (str="") + getNodeByPre(treeNode));
+        System.out.println("中序遍历：" + (str="") + getNodeByMiddle(treeNode));
+        System.out.println("后序遍历：" + (str="") + getNodeByPri(treeNode));
+    }
+
+    /**
+     * 先序遍历
+     * @param node
+     * @return
+     */
+    public static String getNodeByPre(TreeNode node){
+        if(node != null){
+            str += node.getRoot() ;
+
+            if(node.getLeftNode()!=null){
+                getNodeByPre(node.getLeftNode()) ;
+            }
+            if(node.getRightNode()!=null){
+                getNodeByPre(node.getRightNode()) ;
+            }
+        }
+
+        return str ;
+    }
+
+    /**
+     * 先序遍历
+     * @param node
+     * @return
+     */
+    public static String getNodeByMiddle(TreeNode node){
+        if(node != null){
+            if(node.getLeftNode()!=null){
+                getNodeByMiddle(node.getLeftNode()) ;
+            }
+
+            str += node.getRoot() ;
+
+            if(node.getRightNode()!=null){
+                getNodeByMiddle(node.getRightNode()) ;
+            }
+        }
+        return str ;
+    }
+
+    /**
+     * 后序遍历
+     * @param node
+     * @return
+     */
+    public static String getNodeByPri(TreeNode node){
+        if(node != null){
+            if(node.getLeftNode()!=null){
+                getNodeByPri(node.getLeftNode()) ;
+            }
+
+            if(node.getRightNode()!=null){
+                getNodeByPri(node.getRightNode()) ;
+            }
+
+            str += node.getRoot() ;
+        }
+        return str ;
+    }
+}
---
+++ src/tree/test01/TreeNode.java
@@ -0,0 +1,41 @@
+package tree.test01;
+
+/**
+ * 树类
+ */
+
+public class TreeNode {
+    private String root ;           //根
+    private TreeNode leftNode ;     //左子树
+    private TreeNode rightNode ;    //右子树
+
+    public TreeNode(String root,TreeNode leftNode,TreeNode rightNode){
+        this.root = root ;
+        this.leftNode = leftNode ;
+        this.rightNode = rightNode ;
+    }
+
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        this.root = root;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+}
