83ed8fb77b6b7979a83aa5cde0903739ac294426
henucg <1825337674@qq.com>
2019-08-22 10:33:30 +0800

    计算一个日期是当年的第几天

---
+++ src/ID生成器/IDCreator.java
@@ -0,0 +1,8 @@
+package ID生成器;
+
+/**
+ * ID生成器
+ */
+public class IDCreator {
+	
+}
---
+++ src/计算一个日期是当年第几天/Test01.java
@@ -0,0 +1,32 @@
+package 计算一个日期是当年第几天;
+
+/**
+ * 日期格式必须是yyyy-mm-dd
+ * 日期格式校验省略
+ */
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println(getDayOfYear("2019-08-21"));
+	}
+
+	/**
+	 * 计算一个日期是当年的第几天
+	 * @param date yyyy-mm-dd
+	 * @return
+	 */
+	public static int getDayOfYear(String date){
+		int[] days = {0,0,31,59,90,120,151,181,212,243,273,304,334} ;
+
+		int y = Integer.parseInt(date.substring(0,4)) ;
+		int m = Integer.parseInt(date.substring(5,7)) ;
+		int d = Integer.parseInt(date.substring(8,10)) ;
+
+		int leap = 0 ;
+		// 计算是否是闰年
+		if(m>2 && (y%400==0 || (y%4==0 && y%100!=0))){
+			leap++ ;
+		}
+
+		return days[m] + d + leap ;
+	}
+}
