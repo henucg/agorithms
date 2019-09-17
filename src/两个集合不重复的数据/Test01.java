c9af750e2bf9a0f3b14300c4d687d613cc437cba
henucg <1825337674@qq.com>
2019-09-17 16:06:57 +0800

    HashSet

---
+++ src/两个集合不重复的数据/Test01.java
@@ -0,0 +1,25 @@
+package 两个集合不重复的数据;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Set<Integer> a = new HashSet<Integer>() ;
+		a.add(1) ;
+		a.add(2) ;
+
+		Set<Integer> b = new HashSet<Integer>() ;
+		b.add(2) ;
+		b.add(3) ;
+
+		Set<Integer> a1 = new HashSet<Integer>(a) ;
+		Set<Integer> b1 = new HashSet<Integer>(b) ;
+
+		a1.removeAll(b) ;
+		b1.removeAll(a) ;
+
+		System.out.println(a1);
+		System.out.println(b1);
+	}
+}
