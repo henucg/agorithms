068972167f70935130189d22b08f1f1c29e180f4
henucg <1825337674@qq.com>
2020-08-29 16:38:13 +0800

    算法

---
+++ src/LeetCode200/相交链表/ListNode.java
@@ -0,0 +1,10 @@
+package LeetCode200.相交链表;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/LeetCode200/相交链表/Test01.java
@@ -0,0 +1,28 @@
+package LeetCode200.相交链表;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null){
+            return null ;
+        }
+
+        boolean flag = false ;
+        while(headA!=null){
+            ListNode node = headB ;
+            while(node!=null){
+                if(headA.val==node.val){
+                    node = node.next ;
+                    break ;
+                }else {
+                    node = node.next;
+                }
+            }
+            headA = headA.next ;
+        }
+        return null ;
+    }
+}
